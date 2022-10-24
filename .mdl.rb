509bccfd4dff0905f7d6bc8ac343cd7e20c9b8c5
Simon <s.j.branford@bham.ac.uk>
2022-10-24 18:45:38 +0100

    exclude rule, as it dislike MkDocs

--- .mdl.rb
+++ .mdl.rb
@@ -6,5 +6,6 @@ exclude_rule 'MD007' # list indent
 exclude_rule 'MD009' # trailing spaces
 exclude_rule 'MD012' # multiple blank lines
 exclude_rule 'MD013' # line length
+exclude_rule 'MD046' # Code block style. Doesn't like MkDocs admonitions or content tabs
 
 rule 'MD024', :allow_different_nesting => true
