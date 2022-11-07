da23904308523f44da8d94f4421eb7ddbb8c0a5f
James Carpenter <j.j.carpenter@bham.ac.uk>
2022-11-07 23:48:53 +0000

    initial link checker test

---
+++ .github/workflows/link_check.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import os
+import re
+
+intlnk_regex = re.compile(r".*{:(\s*)#(\w+)(\s*)}")
+
+
+def check_file(filepath, link_dict):
+
+    with open(filepath) as fh:
+        for line in fh.readlines():
+            match = intlnk_regex.match(line)
+            if match:
+                if match[2] in link_dict:
+                    if filepath not in link_dict[match[2]]:
+                        link_dict[match[2]].append(filepath)
+                else:
+                    link_dict[match[2]] = [filepath]
+
+
+def main():
+
+    link_dict = {}
+
+    tests_fail = False
+    fail_info = []
+
+    for root, _, files in os.walk('./docs'):
+        for file in files:
+            if file.endswith('.md'):
+                filepath = os.path.join(root, file)
+                check_file(filepath, link_dict)
+
+    for k, v in link_dict.items():
+        if len(v) > 1:
+            tests_fail = True
+            fail_info.append(f"\"{k}\" is used in {len(v)} files: {v}")
+
+    if tests_fail:
+        print("Internal link tests failed!")
+        print('\n'.join(fail_info))
+        raise SystemExit(1)
+
+
+if __name__ == '__main__':
+    main()
