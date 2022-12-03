8e90b8a33c54dcee503f0015c17c5483f5171cea
Simon Branford <s.j.branford@bham.ac.uk>
2022-12-03 09:48:22 +0000

    improve operation

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -7,12 +7,17 @@ from pathlib import Path
 
 import mkdocs_gen_files
 
+
+# remove the problematic py2.py and symlink the py3.py in its place
+Path(f"src/easybuild/tools/py2vs3/py2.py").unlink(missing_ok=True)
+Path(f"src/easybuild/tools/py2vs3/py2.py").symlink_to(Path("py3.py"))
+
 nav = mkdocs_gen_files.Nav()
 
 for path in sorted(Path("src/easybuild/").rglob("*.py")):
     module_path = path.relative_to("src").with_suffix("")
     doc_path = path.relative_to("src").with_suffix(".md")
-    full_doc_path = Path("reference", doc_path)
+    full_doc_path = Path("api", doc_path)
 
     parts = list(module_path.parts)
 
@@ -31,5 +36,5 @@ for path in sorted(Path("src/easybuild/").rglob("*.py")):
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
-with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+with mkdocs_gen_files.open("api/summary.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
